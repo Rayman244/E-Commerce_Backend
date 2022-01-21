983664d3f8a840a2aea7f942a9ab7531d2a694f5
Rayman244 <rayhewitt42@icloud.com>
2022-01-20 19:44:18 -0500

    got starter files imported and started to link to sequalize and mysql

---
+++ models/Category.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      validate: {
+        notNull:true
+      }
+    },
+    category_name:{
+      type: DataTypes.STRING,
+      validate: {
+        notNull:true
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
